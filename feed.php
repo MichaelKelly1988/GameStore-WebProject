c79d33022d92629f6353252e6f5337d9ec7a6eab
Michael Kelly <mikelly1988@gmail.com>
2016-09-06 11:51:55 +0100

    First Commit

---
+++ feed.php
@@ -0,0 +1,22 @@
+	<!DOCTYPE>
+	<html>
+	<head>
+	<title>Calculate the student grade.</title>
+	</head>
+	<body>
+	<h2>Student Grade Information</h2>
+
+	<?php
+	$name = $_POST['name'];
+	$mark = $_POST['address'];
+	$name = $_POST['email'];
+	$mark = $_POST['number'];
+	$name = $_POST['gender'];
+	$mark = $_POST['age'];
+	$name = $_POST['name'];
+	$mark = $_POST['comment'];
+	echo " Hello   " .$name." your mark is  " .$address. "  ";
+	?>
+
+	</body>
+	</html>
